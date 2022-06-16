a28c1991697ce39576f2c2f05189a5231436e8e5
skyh24 <skyhighfeng@gmail.com>
2022-06-17 01:21:40 +0800

    test: call

--- canister/call.sh
+++ canister/call.sh
@@ -2,21 +2,22 @@ dfx canister call live_detect get_token '("aaa")'
 #(
 #  variant {
 #    Ok = record {
-#      token = "aaa";
-#      action = variant { movement = variant { TurnRight } };
-#      active = true;
-#      create_at = 1_654_940_512_557_564_000 : nat64;
+#      action = variant { Move = variant { Mouth } };
+#      active = false;
+#      create_at = 1_655_399_928_768_515_000 : nat64;
+#      scope = "aaa";
 #    }
 #  },
 #)
 
 dfx canister call live_detect detect_start  '("aaa", 0)'
+#(variant { Ok = variant { Move = variant { Mouth } } })
 dfx canister call live_detect detect_start  '("aaa", 1)'
+#(variant { Ok = variant { Speech = "4" } })
 
 dfx canister call live_detect detect_end  '("aaa", variant { Speech = "4" })'
 #(variant { Ok = true })
 dfx canister call live_detect is_alive '("aaa")'
 #(variant { Ok = true })
-# after 60s
-#(variant { Err = variant { TokenExpired } })
+
 
